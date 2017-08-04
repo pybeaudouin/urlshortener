1409f5771d9c759d986c93d895f9f71dfe5daff0
Pierre-Yves Beaudouin <pybeaudouin@users.noreply.github.com>
2017-08-04 00:06:51 -0400

    Change the command line controller with a web controller. Add an embedded Tomcat server.

--- src/main/java/pyb/portfolio/urlshortener/Application.java
+++ src/main/java/pyb/portfolio/urlshortener/Application.java
@@ -1,26 +1,13 @@
 package pyb.portfolio.urlshortener;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	@Autowired
-	private URLController ctrl;
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("**********************");
-		System.out.println("* URL Shortener v0.1 *");
-		System.out.println("**********************");
-		ctrl.loop();
 	}
 
 }
--- src/main/java/pyb/portfolio/urlshortener/URLController.java
+++ src/main/java/pyb/portfolio/urlshortener/URLController.java
@@ -2,96 +2,34 @@ package pyb.portfolio.urlshortener;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import pyb.portfolio.urlshortener.api.v1.URLShortenService;
 
-@Controller
+@RestController
 @Slf4j
 public class URLController {
 
-	private final Scanner SCANNER = new Scanner(System.in);
-	private final Map<Character, MenuItem> menu = new LinkedHashMap<>();
-	private @Autowired URLShortenService urlSrv;
+	@Autowired
+	private URLShortenService urlSrv;
 
-	public URLController() {
-		menu.put('S', new MenuItem('S', "Shorten a URL", () -> shorten()));
-		menu.put('E', new MenuItem('E', "Expand a URL", () -> expand()));
-		menu.put('Q', new MenuItem('Q', "Quit", () -> quit()));
-	}
-
-	private void shorten() {
-		log.info("Shortening URLs");
-		URL url = null;
-		String input = null;
-		while(url == null) {
-			System.out.print("Please enter a URL: ");
-			input = SCANNER.next();
-			// TODO: validate URL
-			try {
-				url = new URL(input);
-			} catch (final MalformedURLException e) {
-				System.err.println(String.format("'%s' isn't a valid URL: %s", input, e.getMessage()));
-			}
+	@RequestMapping(value = "/1", method = RequestMethod.POST)
+	public void shorten(@RequestParam("url") String urlStr) {
+		log.info("Shortening URL " + urlStr);
+		URL url;
+		try {
+			url = new URL(urlStr);
+		} catch (final MalformedURLException e) {
+			log.error(String.format("'%s' isn't a valid URL: %s", urlStr, e.getMessage()));
+			return;
 		}
 		final String shortCode = urlSrv.shortCode(url);
-		System.out.println(String.format("The URL '%s' can be shorten to: %s", input, shortCode));
+		log.info(String.format("The URL '%s' can be shorten to: %s", urlStr, shortCode));
 	}
-
-	private void expand() {
-		log.info("Expanding URLs");
-	}
-
-	private void quit() {
-		System.out.println("Thank you for your service!");
-		SCANNER.close();
-		System.exit(0);
-	}
-
-	void loop() {
-
-		while (true) {
-			for (final MenuItem it : menu.values()) {
-				System.out.println(it);
-			}
-			final String input = SCANNER.next();
-
-			System.out.println(String.format("You typed [%s]", input));
-			if (input.length() != 1) {
-				System.err.println("Please enter a valid choice.");
-				continue;
-			}
-
-			final char choice = Character.toUpperCase(input.charAt(0));
-			if (!menu.containsKey(choice)) {
-				System.err.println("Please enter a valid choice.");
-			} else {
-				menu.get(choice).run();
-			}
-		}
-	}
-
-	@Data
-	private static class MenuItem {
-		private final char key;
-		private final String label;
-		private final Runnable command;
-
-		public void run() {
-			command.run();
-		}
-
-		@Override
-		public String toString() {
-			return key + ") " + label;
-		}
-	}
-
 }
