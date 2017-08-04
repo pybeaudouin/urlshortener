63615bdbc3af31edae70a63bf6a59dbea316f9e0
Pierre-Yves Beaudouin <pybeaudouin@users.noreply.github.com>
2017-08-04 00:44:31 -0400

    Bump version to 0.2; return the short URL instead of its short code.

--- src/main/java/pyb/portfolio/urlshortener/URLController.java
+++ src/main/java/pyb/portfolio/urlshortener/URLController.java
@@ -1,6 +1,7 @@
 package pyb.portfolio.urlshortener;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 import pyb.portfolio.urlshortener.api.v1.URLShortenService;
@@ -18,23 +21,43 @@ import pyb.portfolio.urlshortener.api.v1.URLShortenService;
 @Slf4j
 public class URLController {
 
+	public static final short API_VERSION = 1;
+
 	@Autowired
 	private URLShortenService urlSrv;
 
-	@RequestMapping(value = "/1", method = RequestMethod.POST)
+	@RequestMapping(value = "/" + API_VERSION, method = RequestMethod.POST)
 	public ResponseEntity<String> shorten(@RequestParam("url") String urlStr) {
 		log.info("Shortening URL " + urlStr);
 		URL url;
 		try {
 			url = new URL(urlStr);
 		} catch (final MalformedURLException e) {
-			String error = String.format("Invalid URL: [%s]", urlStr);
+			final String error = String.format("Invalid URL: [%s]", urlStr);
 			log.error(error, e);
 			// SECURITY: don't print the input to avoid Cross-site request forgery
-			return new ResponseEntity<String>("Invalid URL", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>("Invalid URL", HttpStatus.BAD_REQUEST);
 		}
 		final String shortCode = urlSrv.shortCode(url);
-		log.info(String.format("The URL '%s' can be shorten to: %s", urlStr, shortCode));
-		return new ResponseEntity<String>(shortCode, HttpStatus.OK);
+
+		URL shortURL = null;
+		try {
+			shortURL = buildShortURL(shortCode);
+		} catch (final MalformedURLException e) {
+			log.error(String.format(
+					"The URL '%s' can be shorten to the short code %s but there was an error while generating the short URL.",
+					urlStr, shortCode), e);
+		}
+
+		log.info(String.format("The URL '%s' can be shorten to: %s", urlStr, shortURL.toExternalForm()));
+		return new ResponseEntity<>(shortURL.toExternalForm(), HttpStatus.OK);
+	}
+
+	private URL buildShortURL(String shortCode) throws MalformedURLException {
+		final UriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentContextPath();
+		builder.path("/" + API_VERSION);
+		builder.pathSegment("{shortCode}");
+		final URI newUri = builder.buildAndExpand(shortCode).toUri();
+		return newUri.toURL();
 	}
 }
